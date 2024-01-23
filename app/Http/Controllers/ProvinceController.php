1c30a37ab4860bc0698947ae5e907b95f4dada0d
oh-glory <jamesohglory@gmail.com>
2024-01-23 08:31:15 +0100

    yuo

--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Province;
+use App\Models\Region;
+use App\Models\Continent;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class ProvinceController extends Controller
 {
-    //
+    public function getProvincelist()
+    {
+        $data['provinces'] = Province::all();
+        $data['regions'] = Region::all();
+        $data['continents'] = Continent::all();
+
+        return response()->json(['data' => $data, 'message' => 'Provinces fetched successfully']);
+    }
+
+    public function getProvince(Request $request)
+    {
+        $data = Province::where('id', $request->id)->first();
+
+        return response()->json($data);
+    }
+
+    public function createProvince(Request $request)
+    {
+        $this->validate($request, [
+            'description' => 'required',
+            // Add any other validation rules as needed
+        ]);
+
+        try {
+            $province = Province::create($request->all());
+            return redirect()->back()->with(['success' => 'Province created successfully!']);
+        } catch (\Exception $exception) {
+            return redirect()->back()->withErrors(['exception' => $exception->getMessage()]);
+        }
+    }
+
+    public function updateProvince(Request $request)
+    {
+        $id = $request->id;
+
+        $this->validate($request, [
+            'description' => 'required',
+            // Add any other validation rules as needed
+        ]);
+
+        $province = Province::find($id);
+        $province->update($request->all());
+
+        return redirect()->back()->with('success', 'Province updated successfully');
+    }
+
+    public function destroy(Request $request)
+    {
+        $id = $request->id;
+        Province::find($id)->delete();
+
+        return redirect()->back()->with('success', 'Province deleted successfully');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContinentController;
+use App\Http\Controllers\ProvinceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-
+// continent API
 Route::get('/continentslist', [ContinentController::class, 'getContinentList']);
 Route::get('/continents', [ContinentController::class, 'getContinent']);
 Route::post('/create_continent', [ContinentController::class, 'createContinent']);
 Route::post('/upodate_continent', [ContinentController::class, 'updateContinent']);
 Route::get('/delete_continent', [ContinentController::class, 'destroy']);
 
+//Province API
+Route::group(['prefix' => 'province'], function () {
+    Route::get('/', [ProvinceController::class, 'getProvince']);
+    Route::get('/provinceslist', [ProvinceController::class, 'getProvincelist']);
+    Route::post('/create_provinces', [ProvinceController::class, 'createProvince']);
+    Route::put('/update_provinces', [ProvinceController::class, 'updateProvince']);
+    Route::delete('/delete_provinces', [ProvinceController::class, 'destroy']);
+
+});
